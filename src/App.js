717e63733aa6aa0832a8891b62ad0d868011d01b
Samson Kitigo <samsonjesuyon@gmail.com>
2025-01-28 22:44:32 -0600

    testing-deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1>Hello World</h1>
       <p>My name is Samson Kitigo</p>
       <h2>I work</h2>
-      <p>Cut me some slack </p>
+      <p>Cut me some slack!</p>
     </div>
   );
 }
